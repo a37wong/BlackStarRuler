f320e464d19377530bf3299c64872c0a668846dc
ultrapunk <ultra_punk@hotmail.com>
2017-01-23 00:05:32 -0800

    Adding basic earth tile and a single building. Added empty scripts for creating scene.

---
+++ Assets/PlanetScene/Scripts/PlanetMapManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * 
+ * This class is for handling the scene creation and changes
+ * 
+ */
+public class PlanetMapManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
