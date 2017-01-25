809db83ac4b16574a8ce99c68a5c809bf8656ca4
Selina Hu <selinahu@Selinas-MacBook-Pro.local>
2017-01-24 18:44:55 -0800

    added scrolling bounds to galaxy scene

--- Assets/GalaxyScene/Scripts/CameraScroll.cs
+++ Assets/GalaxyScene/Scripts/CameraScroll.cs
@@ -6,7 +6,7 @@ public class CameraScroll : MonoBehaviour {
 	private float minXBound = Screen.width * .1f;
 	private float maxXBound = Screen.width * .9f;
 	private float minYBound = Screen.height * .9f;
-	private float ScrollSpeed = 15f;
+	private float ScrollSpeed = 60f;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,16 +14,16 @@ public class CameraScroll : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.mousePosition.y >= Screen.height * 0.95) {
+		if (Input.mousePosition.y >= Screen.height * 0.95 && this.gameObject.transform.position.y <= 30) {
 			transform.Translate (Vector3.up * Time.deltaTime * ScrollSpeed, Space.World);
 		} 
-		else if (Input.mousePosition.y <= Screen.height * 0.05) {
+		else if (Input.mousePosition.y <= Screen.height * 0.05 && this.gameObject.transform.position.y >= -30) {
 			transform.Translate (Vector3.down * Time.deltaTime * ScrollSpeed, Space.World);
 		}
-		else if (Input.mousePosition.x <= Screen.width * 0.05) {
+		if (Input.mousePosition.x <= Screen.width * 0.05 && this.gameObject.transform.position.x >= -50) {
 			transform.Translate (Vector3.left * Time.deltaTime * ScrollSpeed, Space.World);
 		}
-		else if (Input.mousePosition.x >= Screen.width * 0.95) {
+		else if (Input.mousePosition.x >= Screen.width * 0.95 && this.gameObject.transform.position.x <= 50) {
 			transform.Translate (Vector3.right * Time.deltaTime * ScrollSpeed, Space.World);
 		}
 	}
