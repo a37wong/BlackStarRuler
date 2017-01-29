e870217dfd97dd5475f6faf5cca4846850931d48
ultrapunk <ultra_punk@hotmail.com>
2017-01-28 17:01:50 -0800

    Build out skeleton for gameState and endTurn

---
+++ Assets/Global/Scripts/DataTypes/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Player : MonoBehaviour
+{
+	ArrayList SolarSystemIds;
+
+	public Player()
+	{
+		this.SolarSystemIds = new ArrayList();
+	}
+}
---
+++ Assets/Global/Scripts/GameState/GameState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Class holds all data for a particular play through
+ * 
+ * A separate class is used to initialize a GameState for a new game
+ */
+[Serializable]
+public class GameState : MonoBehaviour
+{
+	public void endTurn()
+	{
+		//go through each player
+		foreach (Player player in PlayerDataStore.control.players)
+		{
+			//add resources from each owned planet
+			//food consumption and population growth on each owned planet
+			//trade good consumption and money earned on each owned planet
+		}
+	}
+}
