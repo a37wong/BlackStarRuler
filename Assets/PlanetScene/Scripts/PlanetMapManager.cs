5985c0502d2214aa9ff826d9a30bd012d11f3254
ultrapunk <ultra_punk@hotmail.com>
2017-01-30 21:46:11 -0800

    Change building types to use a hashtable instead of a flat array to have better control over accessing building types via an integer id

--- Assets/PlanetScene/Scripts/PlanetMapManager.cs
+++ Assets/PlanetScene/Scripts/PlanetMapManager.cs
@@ -151,7 +151,7 @@ public class PlanetMapManager : MonoBehaviour
 		if (currPlanet.buildings.ContainsKey(selectedTile))
 		{
 			int buildingTypeId = (int)currPlanet.buildings[selectedTile];
-			Building selectedBuildingOnTile = BuildingTypes.control.buildingTypes[buildingTypeId];
+			Building selectedBuildingOnTile = (Building)BuildingTypes.control.buildingTypes[buildingTypeId];
 			currentSelectedTile.text = selectedBuildingOnTile.name;
 		}
 		else
